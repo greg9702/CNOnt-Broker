8588227ef7c634db775a3784e42af0b58f568505
greg9702 <grzegorzb9702@gmail.com>
2020-05-16 16:24:59 +0200

    Almost done

--- cluster/setup.sh
+++ cluster/setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-set -e
+echo "Starting docker deamon"
+dockerd &> /dev/null &
+docker ps &> /dev/null
 
-docker ps
+while [ $? -ne 0 ]
+do
+    echo "Waiting for docker deamon..."
+    sleep 1
+    docker ps > /dev/null
+done
+
+echo "Docker deamon ready!"
+
 
 # script creates a Kubernetes cluser from scratch
 # configuration file is cluser-config.yaml
@@ -16,7 +26,10 @@ echo $cluster_name > CLUSTERNAME
 kind delete cluster --name $cluster_name
 kind create cluster --config cluster-config.yaml --name $cluster_name
 
-kind get kubeconfig --name $cluster_name > ../core/kubeconfig
+mkdir secret
+kind get kubeconfig --name $cluster_name > ./secret/kubeconfig
+
+sed -i -e 's|server:.*|server: http://127.0.0.1:8001|g' ./secret/kubeconfig
 
 if [ $create_admin == 1 ]; then
     echo "Admin account created"
@@ -31,7 +44,7 @@ if [ $create_admin == 1 ]; then
      admin_token_name=$(kubectl -n kube-system get serviceaccount admin -o yaml | tail -1 | cut -d":" -f 2 | cut -d" " -f2)
      admin_token=$(kubectl -n kube-system get secret $admin_token_name -o yaml | grep token | head -1 | sed -e 's/.* \(.*\)$/\1/')
      echo "Admin token: "
-     echo $admin_token | base64 --decode
+     echo $admin_token | base64 -d
      echo ""
 else
     echo "Admin account not created"
